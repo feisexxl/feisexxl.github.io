b95ffc38dc46efe776a436e08a1ebf2d5cc6c110
feisexxl <>
2023-02-05 10:37:34 +0800

    h

---
+++ js/plugins/自动换行.js
@@ -0,0 +1,25 @@
+﻿/*:
+ * @plugindesc 自动换行
+ * @author FlyRoc
+ * @target MZ
+ * @help
+ * 实现自动换行小功能。
+ */
+Window_Base.prototype.processCharacter = function (textState) {
+    const c = textState.text[textState.index++];
+    if (c.charCodeAt(0) < 0x20) {
+        this.flushTextState(textState);
+        this.processControlCharacter(textState, c);
+    } else {
+        textState.buffer += c;
+        if (textState.text) {
+            var jc = textState.text.match(/RE/i);
+        } else {
+            var jc = null;
+        }
+        if (textState.x + this.textWidth(c) >= this.innerWidth && jc == null) {
+            this.processNewLine(textState);
+        }
+    }
+};
+
